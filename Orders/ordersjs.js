4914fb4d8565d3f295674a6e11d3aacc0ef0d1b1
Naveena K <naveena713@gmail.com>
2020-10-15 11:44:31 +0530

    navee second commit

--- Orders/ordersjs.js
+++ Orders/ordersjs.js
@@ -1,43 +1,31 @@
-function checkType(that){
-alert("change");
-	var items=document.getElementsByClassName("item");
+alert("Hello");
+
+var itemlist={
+	hotbeverage : ['cappucino' , 'cafelatte' , 'americano' , 'mocha' , 'hotchocolate' , 'expresso' , 'macchiato' , 'hazelnutcappucino' , 'tiramisucappucino' , 'tea' , 'gingertea'],
+	coldbeverage : ['icedcaffelatte', 'icedmocha', 'icedfrappe', 'icedtea', 'icedmilktea'],
+	sandwich : ['caesarsalad', 'blt', 'breakfast1', 'cheesytoast', 'lettucepitapockets', 'asianstyletofu', 'pestoveggie', 'crispyeggplant', 'smokedveggiepanini'],
+	wrap : ['mediterranean', 'thaistyle', 'breakfast2', 'paneer', 'cheesyspinach'],
+	minibite : ['croissant', 'bagelwcreamcheese', 'quesadilla', 'vegetablepastrypuff', 'bakedpitachips', 'cheesygarlicbread'],
+	dessert : ['minichocolavacake', 'miniapplepie', 'baklava', 'hotfudgebrownie', 'caramelbrownie', 'icecreambrownie', 'classiccheesecake', 'triplechococheesecake', 'blueberrycheesecake', 'lemoncheesecake', 'oatmealraisincookie', 'chocochipcookie', 'sugarcookie']
+};
 
 
-var elements = document.getElementsByClassName("testClass1");
-// Iterate through each element that was found
-for(var e = 0; e < elements.length; e++){
-     // Get the inner content via the innerHTML property
-     var content = elements[e].innerHTML;
+var ftype=document.getElementById("type");
+var fitem=document.getElementById("item");
 
-	if(that.value=="hotbeverage")
-	{
-		document.getElementById("hbev").style.display="block";
-		document.getElementsByClassName("item").style.display="block";
-	
-	}
-	if(that.value=="coldbeverage")
-	{
-		document.getElementById("cbev").style.display="block";
-		document.getElementsByClassName("item").style.display="block";
-
-	}
-	if(that.value=="sandwich")
-	{
-		document.getElementById("sand").style.display="block";
-		
-	}
-	if(that.value=="wrap")
-	{
-		document.getElementById("wrap").style.display="block";
-		
-	}
-	if(that.value=="minibite")
-	{
-		document.getElementById("mini").style.display="block";
-		
-	}
-	if(that.value=="dessert")
-	{
-		document.getElementById("dess").style.display="block";
-	}
+for(typ in itemlist)
+{
+	ftype.innerHTML=ftype.innerHTML + '<option>' + typ + '</option>';
 }
+
+ftype.addEventListener('change', function options(e){
+	fitem.innerHTML='';
+	typ=e.target.value;
+	if(typ in itemlist)
+	{
+		for(i=0;i<itemlist[typ].length;i++)
+		{
+			fitem.innerHTML=fitem.innerHTML + '<option>' + itemlist[typ][i] + '</option>';
+		}
+	}
+});
